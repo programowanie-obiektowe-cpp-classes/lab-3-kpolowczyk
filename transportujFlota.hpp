cfbf3078509f1c8eaf4060ea81115538cbc3ca3f
Kamil Polowczyk <kamil.polowczyk.stud@pw.edu.pl>
2024-11-04 08:01:03 +0100

    z3 v1

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,25 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar==0) return 0;
+
+    Stocznia stocznia;
+    unsigned int przetransportowanyTowar=0;
+    unsigned int liczbaZaglowcow=0;
+    while (przetransportowanyTowar<towar)
+    {
+        Statek* statek=stocznia(); //statek-jakiś randomowy statek
+        unsigned int ladunek=statek->transportuj(); //polimorfizm - przechodzimy przez jakiś statek do transportuj
+        przetransportowanyTowar+=ladunek;
+    
+
+     if (dynamic_cast<Zaglowiec*>(statek) != nullptr) {
+            ++liczbaZaglowcow; // Zliczanie żaglowców
+        }
+
+
+
+    delete statek;
+    }
+    return liczbaZaglowcow;
 }
